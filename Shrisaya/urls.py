41fc3970e8dabaea075b73443011d38f77a7681c
patel-darshil <90374796+patel-darshil@users.noreply.github.com>
2021-09-09 13:52:21 +0530

    Update urls.py

--- Shrisaya/urls.py
+++ Shrisaya/urls.py
@@ -23,5 +23,6 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ShrisayaApp.urls')),
-    
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
